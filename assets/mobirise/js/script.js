320a83c15489f5900ea6118308587fec00fb30ae
MoniMed <cci_moni@hotmail.com>
2016-03-28 16:18:44 -0600

    Se integran secciones quienes somos, mision y contacto

--- assets/mobirise/js/script.js
+++ assets/mobirise/js/script.js
@@ -419,7 +419,7 @@
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.9.7';
+        e.innerHTML = '<a href="https://mobirise.com">mobirise.com</a> Mobirise v2.9.10';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
