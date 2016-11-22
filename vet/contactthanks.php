0d08c7439613379968790dab0e43f43800d314d9
update branch <monica.medina@MacBook-Pro-de-monicamedina.local>
2016-11-21 20:44:26 -0600

    contacto

--- vet/contactthanks.php
+++ vet/contactthanks.php
@@ -1,10 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
 
-<head>
-	<title>Mensaje a Animal's Center</title>
-	
-	<link rel="stylesheet" type="text/css" href="style.css" />
-</head>
 
 <body>
 
@@ -22,6 +16,134 @@
 		<p><a href="index.html">Regresa a inicio</a></p>
 	
 	</div>
+
+</body>
+
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--Tags que deben ir al inicio de head antes que otra cosa -->
+  <title>Mensaje a Cl&iacute;nica Animals Center</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet"/>
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet"/>
+  <link href="css/styles.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   <!--[if lt IE 9]>
+     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+   <![endif]-->
+
+<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="images/manifest.json">
+<link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+<header>
+    <div class="head01">
+      <div class="container">
+        <div class="col-md-10 col-xs-10"><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:+5555954709">(55) 5095-4709</a> | <i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:contacto@animalscenter.com.mx">contacto@animalscenter.com.mx</a></div>
+        <div class="col-md-2 col-sm-2 redes-header">
+        <a href="http://www.facebook.com/ClinicaAnimalsCenter/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="http://www.twitter.com/CAnimalsCenter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="head02">
+      <div class="container">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="index.html"><h1>Clinica Animal's Center</h1></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="index.html">Inicio <span class="sr-only">(current)</span></a></li>
+                <li><a href="nosotros.html">Nosotros</a></li>
+                <li><a href="servicios.html">Servicios</a></li>
+                <li class="active"><a href="contacto.html">Contacto</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+      </div>
+    </div>
+</header>
+
+<section class="container contacto">
+  <div class="row">
+    <div class="google-maps">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3760.0157355788183!2d-99.03538678509162!3d19.54093798682288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f00ce5eb90ef%3A0xd79ac053095bd629!2sCuautitlan+2%2C+Cd+Azteca+1ra+Secc%2C+55120+Ecatepec+de+Morelos%2C+M%C3%A9x.!5e0!3m2!1ses-419!2smx!4v1478128239473" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+    <div class="col-sm-12 title">
+      <p>no dudes en comunicarte con nosotros,<br>te atenderemos a la brevedad. <br/>también contáctanos por redes sociales o vía telefónica</p>
+    </div>
+    <div class="clearfix"></div>
+    <!-- <div class="col-sm-12 text">
+      <p>Fundada en el año 2016, Clínica Animal’s Center nace con el objetivo de brindar servicios veterinarios de primer nivel a todos los animales de compañía, siempre bajo la filosofía del bienestar tanto para el propietario como para la mascota.</p>
+    </div> -->
+    <div class="col-sm-12 title">
+      <p>Mensaje enviado <br/>
+puedes regresar a inicio</p>
+    </div>
+    <div class="col-sm-12 text">
+      <p><a href="index.html">Inicio</a></p>
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  <div class="foot01">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-2 col-sm-2 payment"><img src="images/pagos.png" width="100px"/></div>
+        <div class="col-md-8 col-sm-8">
+          <div class="row">
+            <div class="col-md-4 col-sm-4 after"><i class="fa fa-phone" aria-hidden="true"></i>10:00 hrs - 20:00 hrs</div>
+            <div class="col-md-3 col-sm-3 after">Tel. (55) 5095-4709</div>
+            <div class="col-md-4 col-sm-4 correoFooter">contacto@animalscenter.com.mx</div>
+          </div>
+        </div>
+        <div class="col-md-2 col-sm-2 social">
+        <a href="http://www.facebook.com/ClinicaAnimalsCenter/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="http://www.twitter.com/CAnimalsCenter" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="foot02">
+    <p>Cl&iacute;nica Animal's Center 2016 ©  M&eacute;xico. Todos los Derechos Reservados</p>
+  </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script>
+new WOW().init();
+</script>
 
 </body>
 
